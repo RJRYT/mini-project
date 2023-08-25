b768eb18f98489f9f1deb9a0c6de9984a2826ffe
RJRYT <robinjratr@gmail.com>
2023-08-25 17:40:20 +0530

    Update search section and add IDs to elements

--- public/js/custom.js
+++ public/js/custom.js
@@ -4,19 +4,6 @@
 
 (function($) {
     "use strict";
-
-    /* ==============================================
-    AFFIX
-    =============================================== */
-    $('.megamenu').affix({
-        offset: {
-            top: 0,
-            bottom: function() {
-                return (this.bottom = $('.footer').outerHeight(true))
-            }
-        }
-    })
-
     /* ==============================================
     BACK TOP
     =============================================== */
@@ -39,113 +26,6 @@
     $(window).load(function() {
         $("#preloader").on(500).fadeOut();
         $(".preloader").on(600).fadeOut("slow");
-    });
-
-    /* ==============================================
-     FUN FACTS -->
-     =============================================== */
-
-    function count($this) {
-        var current = parseInt($this.html(), 10);
-        current = current + 50; /* Where 50 is increment */
-        $this.html(++current);
-        if (current > $this.data('count')) {
-            $this.html($this.data('count'));
-        } else {
-            setTimeout(function() {
-                count($this)
-            }, 30);
-        }
-    }
-    $(".stat_count, .stat_count_download").each(function() {
-        $(this).data('count', parseInt($(this).html(), 10));
-        $(this).html('0');
-        count($(this));
-    });
-
-    /* ==============================================
-     TOOLTIP -->
-     =============================================== */
-    $('[data-toggle="tooltip"]').tooltip()
-    $('[data-toggle="popover"]').popover()
-
-    /* ==============================================
-     CONTACT -->
-     =============================================== */
-    jQuery(document).ready(function() {
-        $('#contactform').submit(function() {
-            var action = $(this).attr('action');
-            $("#message").slideUp(750, function() {
-                $('#message').hide();
-                $('#submit')
-                    .after('<img src="images/ajax-loader.gif" class="loader" />')
-                    .attr('disabled', 'disabled');
-                $.post(action, {
-                        first_name: $('#first_name').val(),
-                        last_name: $('#last_name').val(),
-                        email: $('#email').val(),
-                        phone: $('#phone').val(),
-                        select_service: $('#select_service').val(),
-                        select_price: $('#select_price').val(),
-                        comments: $('#comments').val(),
-                        verify: $('#verify').val()
-                    },
-                    function(data) {
-                        document.getElementById('message').innerHTML = data;
-                        $('#message').slideDown('slow');
-                        $('#contactform img.loader').fadeOut('slow', function() {
-                            $(this).remove()
-                        });
-                        $('#submit').removeAttr('disabled');
-                        if (data.match('success') != null) $('#contactform').slideUp('slow');
-                    }
-                );
-            });
-            return false;
-        });
-    });
-
-    /* ==============================================
-     CODE WRAPPER -->
-     =============================================== */
-
-    $('.code-wrapper').on("mousemove", function(e) {
-        var offsets = $(this).offset();
-        var fullWidth = $(this).width();
-        var mouseX = e.pageX - offsets.left;
-
-        if (mouseX < 0) {
-            mouseX = 0;
-        } else if (mouseX > fullWidth) {
-            mouseX = fullWidth
-        }
-
-        $(this).parent().find('.divider-bar').css({
-            left: mouseX,
-            transition: 'none'
-        });
-        $(this).find('.design-wrapper').css({
-            transform: 'translateX(' + (mouseX) + 'px)',
-            transition: 'none'
-        });
-        $(this).find('.design-image').css({
-            transform: 'translateX(' + (-1 * mouseX) + 'px)',
-            transition: 'none'
-        });
-    });
-    $('.divider-wrapper').on("mouseleave", function() {
-        $(this).parent().find('.divider-bar').css({
-            left: '50%',
-            transition: 'all .3s'
-        });
-        $(this).find('.design-wrapper').css({
-            transform: 'translateX(50%)',
-            transition: 'all .3s'
-        });
-        $(this).find('.design-image').css({
-            transform: 'translateX(-50%)',
-            transition: 'all .3s'
-        });
     });
 
 })(jQuery);
@@ -230,4 +110,5 @@ const AllFacts = [
     "It would take the caffeine found in 100 cups of coffee, consumed in four hours, to kill an adult.",
     "Once infected with malaria, a person's chances of survival often depend on how quickly they seek treatment"
 ];
-document.getElementById("FactArea").innerHTML = AllFacts[Math.floor(Math.random() * AllFacts.length)];
+let f = document.getElementById("FactArea");
+if(f) f.innerHTML = AllFacts[Math.floor(Math.random() * AllFacts.length)];
